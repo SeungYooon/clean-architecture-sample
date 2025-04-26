a5b26095852350f1d726f569ca48b93704c1e768
SeungYooon <tmddbs4998@naver.com>
2025-04-26 21:17:13 +0900

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "pokemon"
+include(":app")
+include(":data")
+include(":domain")
+include(":feature")
+include(":feature:list")
+include(":feature:favorite")
+include(":feature:detail")
+include(":core")
